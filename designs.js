69bf72dd8ff01929152ab76ec7628cc44d4ead92
wintterroad <winterroaduk@gmail.com>
2017-12-02 23:52:08 +0200

    Added toggle-functionality, added borders, added reset-functionality, edited some styles (margin, border).

--- designs.js
+++ designs.js
@@ -1,3 +1,4 @@
+//Let's make sure that DOM is ready.
 $(document).ready(function() {
 
 // Select color input
@@ -9,34 +10,42 @@ $(document).ready(function() {
   $("#button1").click(function() {
     const inputHeight = $("#input_height").val();
     const inputWidth = $("#input_width").val();
+//Call makegrid with values previously "collected".
     makeGrid(inputHeight, inputWidth);
 });
 
-    function makeGrid(height, width) {
-        // Get rid of the old.
-        canvas.empty();
+//Reset-button functionality: empties the "canvas".
+  $("#button2").click(function() {
+    canvas.empty();
+});
 
-        for (let i = 0; i < height; i++) {
-          // Create as many rows as the height
-          const new_row = $("<tr></tr>");
-          (canvas).append(new_row);
+//Toggle Grid Size Picker, when it is not needed.
+  $( "#toggle" ).click(function() {
+    $("#sizePicker").toggle(function() {
+  });
+});
 
-          for (let j = 0; j < width; j++) {
-            // Create as many columns as there is width. Add columns to rows.
-            const col = $("<td></td>");
-            (new_row).append(col);
-          }
-        }
+  function makeGrid(height, width) {
+    // Get rid of the old.
+    canvas.empty();
+
+    // Create as many rows as the height
+    for (let i = 0; i < height; i++) {
+      const new_row = $("<tr></tr>");
+      (canvas).append(new_row);
+
+    // Create as many columns as there is width. Add columns to rows.
+    for (let j = 0; j < width; j++) {
+      const col = $("<td></td>");
+      (new_row).append(col);
       }
-
-      $(canvas).on( 'click', 'td', function() {
-
-        const color = colorPicker.val();
-        $(this).css("background-color", color);
-
-      });
+    }
+  }
+  //When td-element is clicked, colorPicker values is checked and saved in color variable and the td's background-color values is set same as the color. 
+  $(canvas).on( 'click', 'td', function() {
+      const color = colorPicker.val();
+      $(this).css("background-color", color);
+  });
 
 
 });
-//event listener for colors (drawing) and the functionality.
-//empty canvas function and reset button. Maybe save?
